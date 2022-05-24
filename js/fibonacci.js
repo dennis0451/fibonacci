eada47e32350ffb423d6ebc4706457089b112197
Dennis Corral <dennis.corral@outlook.com>
2022-05-24 15:07:01 -0400

    completed fibonacci algo

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,12 @@
 const fibonacci = (num) => {
-    
-}
+    let a = 0, b = 1, sum = 0;
 
+     for(let i = 1; i < num; i++){ //Loop to establish fibonacci sequence using arbitrary number '100'
+        sum = a + b;
+        a = b;  //set variables for next loop of fibonacci sequence
+        b = sum;
+
+    }
+    return sum;
+}
 module.exports = {fibonacci}
