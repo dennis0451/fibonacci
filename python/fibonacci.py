bf944a84e89e09e8802020bfb36b1b06315f19b6
Dennis Corral <dennis.corral@outlook.com>
2022-05-25 16:41:32 -0400

    non recursive solution

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,13 @@
 def fibonacci(n):
-  pass
-
-
+  sum = 0
+  first_Num = 0
+  second_Num = 1
+ 
+  for i in range(1,n):
+      sum = first_Num + second_Num
+      first_Num = second_Num
+      second_Num = sum
+      
+      # print(sum)
+  return sum
+print(fibonacci(45))
